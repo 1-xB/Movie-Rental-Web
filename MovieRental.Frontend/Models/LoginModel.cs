a2cf7b37449fc2ee5ea8c626114c882ab2be3da3
Piotr Zarzycki <bobonuz@protonmail.com>
2025-02-07 18:51:44 +0100

    Add Bootstrap for styling, implement navigation layout, and create login/register pages

---
+++ MovieRental.Frontend/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRental.Frontend.Models;
+
+public class LoginModel
+{
+    [Required, StringLength(255)] public string UsernameOrMail { get; set; }
+    [Required, StringLength(30), DataType(DataType.Password), MinLength(6)] public string Password { get; set; }
+}
---
+++ MovieRental.Frontend/Models/RegisterModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRental.Frontend.Models;
+
+public class RegisterModel
+{
+    [Required, StringLength(30), MinLength(3)] public string Username { get; set; }
+    [Required, StringLength(255), EmailAddress] public string Mail { get; set; }
+    [Required, StringLength(255), DataType(DataType.Password), MinLength(6)] public string Password { get; set; }
+    [Required, StringLength(255), DataType(DataType.Password), Compare("Password", ErrorMessage = "Passwords do not match."), MinLength(6)]
+    public string ConfirmPassword { get; set; }
+}
