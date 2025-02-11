cbf2801c0ec6f6214f0be0df67bc85c402691494
Piotr Zarzycki <bobonuz@protonmail.com>
2025-02-11 20:39:50 +0100

    Add Movies page with movie and genre models, and enhance styling for movie cards

---
+++ MovieRental.Frontend/Models/GenreModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieRental.Frontend.Models;
+
+public class GenreModel
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
---
+++ MovieRental.Frontend/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieRental.Frontend.Models;
+
+public class MovieModel
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public int GenreId { get; set; }
+    public DateOnly ReleaseYear { get; set; }
+
+    public int TotalCopies { get; set; }
+    public int AvailableCopies { get; set; }
+}
