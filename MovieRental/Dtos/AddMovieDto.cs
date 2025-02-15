4846281c08943f40bb33ef4a5649dfec4d1d9e35
Piotr Zarzycki <bobonuz@protonmail.com>
2025-02-15 19:04:43 +0100

    Enhance MoviesSearch page with improved UI elements; add price and available copies display; update AddMovieDto to include price and available copies properties

--- MovieRental/Dtos/AddMovieDto.cs
+++ MovieRental/Dtos/AddMovieDto.cs
@@ -6,8 +6,10 @@ public class AddMovieDto
 {
     public required string Title { get; set; }
     public required string Description { get; set; }
+    public double Price { get; set; }
     public int GenreId { get; set; }
     public DateOnly ReleaseYear { get; set; }
 
     public int TotalCopies { get; set; }
+    public int AvailableCopies { get; set; }
 }
