231182ff5f86d321b62b6a2a1a7a072673c27daf
Piotr Zarzycki <bobonuz@protonmail.com>
2025-02-20 21:35:20 +0100

    Add Checkout page for movie rentals; implement rental form with date validation and price calculation; update MoviesSearch page to navigate to Checkout

---
+++ MovieRental.Frontend/Dtos/RentMovieDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRental.Frontend.Dtos;
+
+public class RentMovieDto
+{
+    public int MovieId { get; set; }
+    [Required] [Range(typeof(DateOnly), "2023-01-01", "2050-12-31", ErrorMessage = "Date out of range.")] public DateOnly ReturnDate { get; set; }
+}
